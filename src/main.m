2cd6d420f9562e1e80c83fbdae6f2710c8489bf8
Pearl Li <pearlli@sas.upenn.edu>
2016-04-06 01:19:24 -0400

    Replace alpha = 0.1 with 0.2

--- src/main.m
+++ src/main.m
@@ -15,7 +15,7 @@ writetable(struct2table(aggregate_results), 'results/implied-rate-summary/aggreg
 results5 = implied_rates('cex-bondholders', 2,   phi_d, nu_d);
 results6 = implied_rates('cex-bondholders', 1,   phi_d, nu_d);
 results7 = implied_rates('cex-bondholders', 0.5, phi_d, nu_d);
-results8 = implied_rates('cex-bondholders', 0.1, phi_d, nu_d);
+results8 = implied_rates('cex-bondholders', 0.2, phi_d, nu_d);
 cex_bondholders_results = [results5, results6, results7, results8];
 writetable(struct2table(cex_bondholders_results), 'results/implied-rate-summary/cex-bondholders.csv');
 
@@ -23,6 +23,6 @@ writetable(struct2table(cex_bondholders_results), 'results/implied-rate-summary/
 results9 = implied_rates('cex-nonbondholders', 2,   phi_d, nu_d);
 results10 = implied_rates('cex-nonbondholders', 1,   phi_d, nu_d);
 results11 = implied_rates('cex-nonbondholders', 0.5, phi_d, nu_d);
-results12 = implied_rates('cex-nonbondholders', 0.1, phi_d, nu_d);
+results12 = implied_rates('cex-nonbondholders', 0.2, phi_d, nu_d);
 cex_nonbondholders_results = [results9, results10, results11, results12];
 writetable(struct2table(cex_nonbondholders_results), 'results/implied-rate-summary/cex-nonbondholders.csv');
